d21f61dd4ada01b8516848f10d9f9a1623dceff5
Acrux107 <vkizilovmellodesign@gmail.com>
2024-10-13 19:41:03 +0300

    first commit

---
+++ prog/mydb/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Users(models.Model):
+    login = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+
+    def __str__(self) -> str:
+        return self.login
